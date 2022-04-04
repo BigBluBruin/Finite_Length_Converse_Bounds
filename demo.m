c62c83641a35ffb71503b5bb1181147b5152d843
Linfang <46692055+BigBluBruin@users.noreply.github.com>
2022-04-04 14:15:00 -0700

    add Gonzalo's bound

--- demo.m
+++ demo.m
@@ -1,32 +1,23 @@
-%--------------------------------------------------------------------------
-%  This script recovers SP59 bound in Fig.1 in the following paper:
-%
-% A. Valembois and M. P. C. Fossorier, "Sphere-packing bounds 
-% revisited for moderate block lengths," in IEEE Transactions 
-% on Information Theory, vol. 50, no. 12, pp. 2998-3014, Dec. 
-% 2004, doi: 10.1109/TIT.2004.838090.
-%
-%
-%--------------------------------------------------------------------------
 
-
+addpath("Gonzalo_lower_bound/","SP59/")
+%%
 clc
 clear
-
 k=64;
 n=128;
 R= k/n;
 snr_decible = 2;
-
-addpath("../../Information-Bottleneck-toolbox/");
-Figuresetting;
 snr_val = 0:0.1:3;
-[FER] = Shannon_SP59(n,k,snr_val);
+[FER_Sh] = Shannon_SP59(n,k,snr_val);
+[FER_Go]=pc_awgn_lower_bound(n,k/n,snr_val);
 
+%%
 figure
-semilogy(FER(:,2),FER(:,4));
+semilogy(FER_Sh(:,2),FER_Sh(:,4),"DisplayName","Shannon'59 SBP");
 hold on 
-xlabel("SNR=10 $\log_{10}\frac{E_b}{N_0}$ (dB)","Interpreter","latex");
+semilogy(FER_Go(:,2),FER_Go(:,4),"DisplayName","Gonzalo'S Converse Bound");
+xlabel("$\frac{E_b}{N_0}$(dB)","Interpreter","latex");
 ylabel("FER");
+legend show
 grid on
 
